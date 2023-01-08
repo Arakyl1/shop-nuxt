2903bff073235355902c50432239711c3db0cae7
Arakyl1 <yura.melnichuk.2015@mail.ru>
2023-01-08 17:55:43 +0300

    fix

--- composables/useAuth.ts
+++ composables/useAuth.ts
@@ -1,131 +1,132 @@
-import { userActive, accessToken, alertContent } from "~~/pinia/store";
-import jwtDecode from "jwt-decode";
+// import jwtDecode from "jwt-decode";
+// import { userActive, accessToken, alertContent } from "~~/pinia/store";
 
 export default () => {
 
-    const updateUser = (data: object | null) => {
-        const user = userActive();
-        user.updateActiveUser(data)
-    }
-    const updateAccess = (data: string) => {
-        const access = accessToken();
-        access.updateAccessToken(data)
-    }
+    // const updateUser = (data: object | null) => {
+    //     const user = userActive();
+    //     user.updateActiveUser(data)
+    // }
+    // const updateAccess = (data: string) => {
+    //     const access = accessToken();
+    //     access.updateAccessToken(data)
+    // }
 
-    const register = (event: Event) => {
-        return new Promise<void>(async(resolve, reject) => {
-            try {
-                const data = await $fetch('/api/auth/register', {
-                    method: "POST",
-                    body: event
-                })
+    // const register = (event: Event) => {
+    //     return new Promise<void>(async(resolve, reject) => {
+    //         try {
+    //             const data = await $fetch('/api/auth/register', {
+    //                 method: "POST",
+    //                 body: event
+    //             })
               
-                updateUser(data.user)
-                updateAccess(data.access_token)
-                resolve(true)
-            } catch (error) {
-                reject(error.statusMessage)
-            }
-        })
+    //             updateUser(data.user)
+    //             updateAccess(data.access_token)
+    //             resolve(true)
+    //         } catch (error) {
+    //             reject(error.statusMessage)
+    //         }
+    //     })
         
-    }
+    // }
 
-    const login = async (event: Event) => {
-        return new Promise<void>(async(resolve, reject) => {
-            try {
-                const data= await $fetch('/api/auth/login', {
-                    method: 'POST',
-                    body: event
-                })
+    // const login = async (event: Event) => {
+    //     return new Promise<void>(async(resolve, reject) => {
+    //         try {
+    //             const data= await $fetch('/api/auth/login', {
+    //                 method: 'POST',
+    //                 body: event
+    //             })
 
-                updateUser(data.user)
-                updateAccess(data.access_token)
-                resolve(true)
-            } catch (error) {
-                reject(error.statusMessage)
-            }
-        })
-    }
+    //             updateUser(data.user)
+    //             updateAccess(data.access_token)
+    //             resolve(true)
+    //         } catch (error) {
+    //             reject(error.statusMessage)
+    //         }
+    //     })
+    // }
 
-    const logout = () => {
-        return new Promise<void>(async(resolve, reject) => {
-            try {
-                await $fetch('/api/auth/logout', {method: "POST"})
-                updateUser(null)
-                updateAccess('')
-                resolve(true)
-            } catch (error) {
-                reject(error.statusMessage)
-            }
-        })
-    }
+    // const logout = () => {
+    //     return new Promise<void>(async(resolve, reject) => {
+    //         try {
+    //             await $fetch('/api/auth/logout', {method: "POST"})
+    //             updateUser(null)
+    //             updateAccess('')
+    //             resolve(true)
+    //         } catch (error) {
+    //             reject(error.statusMessage)
+    //         }
+    //     })
+    // }
 
-    const initAuth = async() => {
-        try {
-            await initRefrechToken();
-            await getUset()
-            reRefrechAccessToken()
-        } catch (error) {
-            return error
-        }
-    }
+    // const initAuth = async() => {
+    //     try {
+    //         await initRefrechToken();
+    //         await getUset()
+    //         reRefrechAccessToken()
+    //     } catch (error) {
+    //         return error
+    //     }
+    // }
 
-    const initRefrechToken = () => {
-        return new Promise<void>(async(resolve, reject) => {
-            try {
-                const data = await $fetch('/api/auth/refrech')
-                updateAccess(data)
-                resolve(true)
-            } catch (error) {
-                reject(error.statusMessage)
-            }
-        })
-    }
+    // const initRefrechToken = () => {
+    //     return new Promise<void>(async(resolve, reject) => {
+    //         try {
+    //             const data = await $fetch('/api/auth/refrech')
+    //             updateAccess(data)
+    //             resolve(true)
+    //         } catch (error) {
+    //             reject(error.statusMessage)
+    //         }
+    //     })
+    // }
 
-    const reRefrechAccessToken = () => {
-        const access = accessToken();
-        if (!access.accessToken) {
-            return
-        }
-        const jwt = jwtDecode(access.accessToken)
+    // const reRefrechAccessToken = () => {
+    //     const access = accessToken();
+    //     if (!access.accessToken) {
+    //         return
+    //     }
+    //     const jwt = jwtDecode(access.accessToken)
         
-        const newRefrecmTime = jwt.exp - 60000
-        setTimeout(() => {
-            initRefrechToken()
-            reRefrechAccessToken()
-        }, newRefrecmTime)
+    //     const newRefrecmTime = jwt.exp - 60000
+    //     setTimeout(() => {
+    //         initRefrechToken()
+    //         reRefrechAccessToken()
+    //     }, newRefrecmTime)
         
-    }
+    // }
 
-    const getUset = () => {
-        return new Promise<void>(async(resolve, reject) => {
-            try {
-                const access = accessToken();
-                const data = await $fetch('/api/auth/user', {
-                    headers: {
-                        method: "GET",
-                        Authorization: `Bearer ${access.accessToken}`
-                    }
-                })
-                updateUser(data)   
-                resolve(true)
-            } catch (error) {
-                reject(error.statusMessage)
-            }
-        })
-    }
+    // const getUset = () => {
+    //     return new Promise<void>(async(resolve, reject) => {
+    //         try {
+    //             const access = accessToken();
+    //             const data = await $fetch('/api/auth/user', {
+    //                 headers: {
+    //                     method: "GET",
+    //                     Authorization: `Bearer ${access.accessToken}`
+    //                 }
+    //             })
+    //             updateUser(data)   
+    //             resolve(true)
+    //         } catch (error) {
+    //             reject(error.statusMessage)
+    //         }
+    //     })
+    // }
 
-    const getUsetInfo = async(event) => {
-        try {
-            const data = await $fetch('/api/auth/userInfo', {
-                method: 'POST',
-                body: event
-            })
-            return data
-        } catch (error) {
-            console.log(error);
-        }
-    }
+    // const getUsetInfo = async(event) => {
+    //     try {
+    //         const data = await $fetch('/api/auth/userInfo', {
+    //             method: 'POST',
+    //             body: event
+    //         })
+    //         return data
+    //     } catch (error) {
+    //         console.log(error);
+    //     }
+    // }
 
-    return { login, initAuth, logout, register, getUsetInfo }
+    // return { login, initAuth, logout, register, getUsetInfo }
+    return true
 }
