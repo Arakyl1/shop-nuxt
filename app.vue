22640c5efd1bc68f75f97a2e4a2128f760963796
Arakyl1 <yura.melnichuk.2015@mail.ru>
2023-01-08 04:53:16 +0300

    add:[content, pinia]

--- app.vue
+++ app.vue
@@ -1,5 +1,96 @@
 <template>
   <div>
-    hellow
+    <!-- <div
+    class="fixed top-0 left-0 w-full bg-black-700 z-40 ap0__mask"
+    :class="[{ active: store.active }]"
+    @click="store.updateActive(false)"></div>
+    <div v-if="size.width > 0">
+    <LazyHeaderMain  v-if="size.width >= 768" class="mb-4"/>
+      <HeaderMainMobaile v-else class="mb-4"/>
+    </div>
+    <div class="max-w-7xl mx-auto px-4 sm:px-3" >
+      <Transition name="path">
+        <OtherElsePath v-if="$route.path !== '/'"/>
+      </Transition>
+      <NuxtPage>
+      </NuxtPage>
+    </div>
+    <div v-if="size.width > 0">
+    <LazyFooter v-if="size.width >= 768" />
+    <LazyFooterMobaile v-else/>
+  </div>
+  <Transition name="alert">
+    <OtherElseAlert/>
+  </Transition> -->
+  <div class="hidden md:grid-cols-1 md:gap-y-6 md:mb-6"></div>
   </div>
 </template>
+
+<script setup lang="ts">
+// import { windowMask, containerSize } from "@/pinia/store";
+// import { storeToRefs } from "pinia";
+
+// const store = windowMask()
+// const containerFunc = containerSize()
+// const { size } = storeToRefs(containerFunc)
+// const { initAuth } = useAuth()
+
+// onBeforeMount(async() => {
+//   initAuth()
+
+//   containerFunc.updateSize(window)
+//   window.addEventListener('resize', () => containerFunc.updateSize(window))
+// })
+
+</script>
+
+<style lang="css">
+@import './assets/Stylesheets/main.css';
+.ap0__mask{
+  transition-delay: 0.22s;
+  height: 0;
+  opacity: 0;
+}
+.ap0__mask.active{
+  height: 100vh;
+  transition-delay: 0s;
+  opacity: 0.8;
+}
+
+.path-enter-active ,
+.path-leave-active {
+  transition: all 0.3s ease-in-out;
+}
+
+
+.path-enter-from,
+.path-leave-to{
+  transform: translateX(30px);
+  opacity: 0;
+}
+
+.page-transition-enter-active{
+  transition: all 0.15s ease-out;
+}
+
+.page-transition-leave-active {
+  transition: all 0.15s ease-in-out;
+}
+
+.page-transition-enter-from,
+.page-transition-leave-to{
+  transform: translateX(20px);
+  opacity: 0;
+}
+
+.alert-enter-active,
+.alert-leave-active {
+  transition: all 0.25s ease-out;
+}
+
+.alert-enter-from,
+.alert-leave-to{
+  opacity: 0;
+  transform: translateY(-20px);
+}
+</style>
