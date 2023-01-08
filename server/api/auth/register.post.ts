3371456c2ae1a4c9d30e313b0bf44f35164e2d68
Arakyl1 <yura.melnichuk.2015@mail.ru>
2023-01-08 18:57:50 +0300

    sd

--- server/api/auth/register.post.ts
+++ server/api/auth/register.post.ts
@@ -6,49 +6,49 @@ import { createRefrechToken } from "~~/server/db/refrechTokem";
 
 
 export default defineEventHandler(async(event: H3Event) => {
-    // const body = await readBody(event)
+    const body = await readBody(event)
 
-    // const { username, email, password, repeartPassword } = body
+    const { username, email, password, repeartPassword } = body
     
-    // if (!username || !email || !password || !repeartPassword) {
-    //     return sendError(event, createError({
-    //         statusCode: 400,
-    //         statusMessage: "Invalid params"
-    //     }))
-    // }
-    // if (password !== repeartPassword) {
-    //     return sendError(event, createError({
-    //         statusCode: 400,
-    //         statusMessage: "Password do not match"
-    //     }))
-    // }
-    // const userData = {
-    //     username,
-    //     email,
-    //     password,
-    //     profileImage: "https://picsum/200/200"
-    // }
+    if (!username || !email || !password || !repeartPassword) {
+        return sendError(event, createError({
+            statusCode: 400,
+            statusMessage: "Invalid params"
+        }))
+    }
+    if (password !== repeartPassword) {
+        return sendError(event, createError({
+            statusCode: 400,
+            statusMessage: "Password do not match"
+        }))
+    }
+    const userData = {
+        username,
+        email,
+        password,
+        profileImage: "https://picsum/200/200"
+    }
 
-    // // const user = await createUser(userData)
-    // // if (!user) {
-    // //     createAndSendError(event, 404, "Error when creating")
-    // // }
+    const user = await createUser(userData)
+    if (!user) {
+        createAndSendError(event, 404, "Error when creating")
+    }
 
-    // // Generate Token
+    // Generate Token
 
     // const { accessToken } = await generateTokens(userData)
 
-    // // await createRefrechToken({
-    // //     token: refrechToken,
-    // //     userId: user.id
-    // // })
+    // await createRefrechToken({
+    //     token: refrechToken,
+    //     userId: user.id
+    // })
 
-    // // sendRefrechToken(event, refrechToken)
+    // sendRefrechToken(event, refrechToken)
 
 
-    // return {
-    //     access_token: accessToken,
-    //     // user: user
-    // }
-    return true
+    return {
+        //access_token: accessToken,
+        user: user
+    }
+    // return true
 })
