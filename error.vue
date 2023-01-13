5b5dade578728a484c6426f82a54943533680702
Arakyl1 <yura.melnichuk.2015@mail.ru>
2023-01-13 06:28:26 +0300

    add:error_template

---
+++ error.vue
@@ -0,0 +1,15 @@
+<template>
+    <div class="h-screen flex justify-center items-center">
+        <ButtomStandart class="bg-blue-500 text-xl text-white"
+        @click="handleError">
+            Перейти на главную
+        </ButtomStandart>
+    </div>
+</template>
+
+<script setup>
+const props = defineProps({
+  error: Object
+})
+const handleError = () => clearError({ redirect: '/' })
+</script>
