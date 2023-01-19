64d3732c48c4a8fb7275161a257eb3a0a421d662
Arakyl1 <yura.melnichuk.2015@mail.ru>
2023-01-19 06:43:27 +0300

    update:fun_copyLink

---
+++ components/Buttom/Share.vue
@@ -0,0 +1,21 @@
+<template>
+<ButtomStandart @click="copyLink">
+    <IconsShare/>
+</ButtomStandart>
+</template>
+
+<script setup lang="ts">
+import IconsShare from '~~/components/Icon/IconsShare.vue';
+import { alertContent } from "@/pinia/store";
+
+const alertContentFun = alertContent()
+
+async function copyLink() {
+    const permissionToUseClipboard = await navigator.permissions.query({ name: "clipboard-write" })
+    if (permissionToUseClipboard.state === "granted" || permissionToUseClipboard.state === "prompt") {
+        navigator.clipboard.writeText(window.location.href)
+        alertContentFun.updateContent('Ссылка скопирована')
+    }
+}
+addEventListener('')
+</script>
