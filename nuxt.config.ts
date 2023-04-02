7fd4512e7d10049d0f760384e0f4fd30d4d6973a
Arakyl1 <yura.melnichuk.2015@mail.ru>
2023-04-02 15:42:49 +0300

    d

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -31,5 +31,12 @@ export default defineNuxtConfig({
   },
   routeRules: {
     '/delivery': { swr: true }
+  },
+  vite: {
+    resolve: {
+      alias: {
+        ".prisma/client/index-browser": "./node_modules/.prisma/client/index-browser.js"
+      }
+    }
   }
 });
