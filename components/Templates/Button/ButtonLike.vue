4d191b2d4cb833404da7008fb21d3d42a7c04336
yura <yuriy.melnichuk.1998@gmail.com>
2024-01-02 02:11:17 +0300

    add button like

---
+++ components/Templates/Button/ButtonLike.vue
@@ -0,0 +1,35 @@
+<template>
+    <Button v-bind="{ ...props }" :appearance="'red-icon'" :icon-left="{ key: 'like', size: '28_28' }" :class="rootClass"/>
+</template>
+<script setup lang="ts">
+import { default as Button, type Props as ButtonProps} from "@/components/UI/Button/Button.vue";
+
+interface Props extends ButtonProps {}
+
+const props = defineProps<Props>()
+const className = useCssModule()
+
+const rootClass = computed(() => {
+    return {
+        [className['button']]: true,
+        [className['active']]: props.active
+    }
+})
+</script>
+
+<style lang="css" module>
+.button {
+    --fill-color: var(--white);
+}
+.button g {
+    stroke: var(--gray-300);
+    stroke-width: 1.5px;
+}
+*:hover > .button g,
+.button:hover g {
+    stroke: var(--red-300);
+}
+.button.active g {
+    stroke: var(--red-500);
+}
+</style>
