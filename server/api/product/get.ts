85e48ecc9342d42cb7160e6d64becbeb96a1d7a5
yura <yuriy.melnichuk.1998@gmail.com>
2024-01-02 02:03:15 +0300

    update get request for product

--- server/api/product/get.ts
+++ server/api/product/get.ts
@@ -1,219 +1,250 @@
 import { H3Event } from "h3"
 import prisma from "~~/server/db"
 import { Prisma } from "@prisma/client";
-import { KeysMatchingWrite } from "~~/type/intex";
+import { getWhereParamsCons, initFindParams, parceIncludeParams, parceOrderByParams, parseOtherArgs, parseSearchParams } from "@/server/utils/parseUrl";
+import { selectAttridute, selectCharacteristic, selectImage, selectComment, selectProductCard, selectCharacteristicItem } from "@/server/utils/selectData";
+import { KeysMatchingWrite, getModelName } from "@/type/intex";
+
+type modelName = getModelName<'ProductCard'>
+const modelName: Uncapitalize<modelName> = 'productCard'
+type ThisMainTypeWhereInput = Prisma.ProductCardWhereInput
+type ThisMainTypeSelect = Prisma.ProductCardSelect
+type ThisMainTypeInclude = Prisma.ProductCardInclude
+
+const propertyElem: Array<keyof ThisMainTypeWhereInput> = ['name','art','itemArt','itemMod']
+const propertyElemOrderBy: Array<keyof ThisMainTypeWhereInput> = ['id','name','discount','createAt','price','quantity','views']
+type PropertyElemInt = KeysMatchingWrite<ThisMainTypeWhereInput, Prisma.IntFilter>
+const PropertyElemInt: PropertyElemInt[] = ['id','discount','price','quantity','views']
+type PropertyElemString = KeysMatchingWrite<ThisMainTypeWhereInput, Prisma.StringFilter>
+const PropertyElemString: PropertyElemString[] = ['name','art','description','itemArt','itemMod']
+type PropertyElemDate = KeysMatchingWrite<ThisMainTypeWhereInput, Prisma.DateTimeFilter>
+const PropertyElemDate: PropertyElemDate[] = ['createAt']
+type keyPropElemRelation = keyof Pick<ThisMainTypeInclude, 'attribute' | 'characteristic' | 'image' | 'reviews'>
+const keyPropElemRelation: keyPropElemRelation[] = ['attribute','characteristic','image','reviews']
 
 
+type GG<T extends keyof ThisMainTypeSelect> = { [P in T]: { [L in keyof ThisMainTypeSelect[P]]: any } }
+const includeElemSelectParams: ThisMainTypeInclude = {
+    'attribute': { select : { ...selectAttridute() } },
+    'characteristic': { select: { ...selectCharacteristic({ 'content': { 'select': { ...selectCharacteristicItem() } } }) } },
+    'image': { select: { ...selectImage() } },
+    'reviews': { select: { ...selectComment() } }
+}
 
-const propertyCard: Array<keyof Prisma.ProductCardWhereInput> = ['art', 'createAt', 'description', 'discount', 'id', 'itemArt', 'itemMod', 'name', 'price', 'quantity', 'views']
+const ElemFullDataKey: ThisMainTypeSelect = {
+    id: true,
+    'art': true,
+    'description': true,
+    'discount': true,
+    'itemArt': true,
+    'itemMod': true,
+    'name': true,
+    'price': true,
+    'quantity': true,
+    'views': true,
+    'createAt': true,
+    ...includeElemSelectParams
+}
 
-type WhereIntFilterKey = (keyof Prisma.IntFilter)
-const WhereIntFilterKey: WhereIntFilterKey[] = ['equals', 'not', 'in', 'notIn', 'lt', 'lte', 'gt', 'gte',]
-type PropertyProductCardInt = KeysMatchingWrite<Prisma.ProductCardWhereInput, Prisma.IntFilter>
-const PropertyProductCardInt: PropertyProductCardInt[] = ['discount', 'id', 'price', 'quantity', 'views']
+function initProductCardFindParams(...arg: Parameters<initFindParams>) {
+    const key = arg[0]
+    const value = arg[1]
+    const addParams = arg[2]
+    const addError = arg[3]
 
+    const _key = key as PropertyElemInt | PropertyElemString | PropertyElemDate |
+        'limit' | 'page' | 'include' | 'orderBy' | 'categor' | 'search' | 'maker' | 'rating' | 'other' | `chr-${string}`
 
-function initWhereParams(query: QueryObject) {
-    const findParams = Prisma.validator<Prisma.ProductCardFindManyArgs>()({
-        where: {
-            'AND': [],
-            // OR: [],
-        },
-        take: 12,
-        skip: 0,
-        orderBy: []
-    })
+    switch (_key) {
+        case 'categor': {
+            const _value = { attribute: { some: { id: parseInt(value as string) } } }
+            addParams('where', _value)
+            break;
+        }
+        case 'search': {
+            const res = parseSearchParams(value, propertyElem)
+            if (res.OR.length) {
+                addParams('where', res)
+            }
+            break;
+        }
+        case 'limit': {
+            const resParse = parseOtherArgs('take' as never, value)
+            console.log(resParse)
+            if (resParse.error.length) {
+                resParse.error.forEach(_ => addError(_))
+            } else {
+                addParams('take', resParse.data.take)
+            }
+            break;
+        }
+        case 'page': {
+            const resParse = parseOtherArgs('skip' as never, value)
+            if (resParse.error.length) {
+                resParse.error.forEach(_ => addError(_))
+            } else {
+                addParams('skip', resParse.data.skip)
+            }
+            break;
+        }
 
-    // new parser
-    for (const key in query) {
-        if (Object.prototype.hasOwnProperty.call(query, key)) {
-            const value = query[key];
-            switch (true) {
-                case key === 'categor': {
-                    findParams.where.AND.push({ attribute: { some: { id: parseInt(value as string) } } } as never)
-                    break;
+        case 'orderBy': {
+            const res = parceOrderByParams(value, propertyElemOrderBy)
+            if (res.error.length) {
+                res.error.forEach(_ => addError(_))
+            } else {
+                addParams('orderBy', res.data)
+            }
+            break;
+        }
+        case 'include': {
+            const res = parceIncludeParams<typeof includeElemSelectParams>(value, includeElemSelectParams)
+            if (res.error.length) {
+                res.error.forEach(_ => addError(_))
+            } else {
+                addParams('include', res.data)
+            }
+            break
+        }
+        case 'maker': {
+            const _value = value.split(',').map(_ => ({
+                attribute: { some: {
+                    value: { contains: _, mode: 'insensitive' },
+                    type: 'MAKER'
+                } }
+            }))
+            addParams('where', { 'OR': _value })
+            break;
+        }
+        case 'rating': {
+            const _value = { reviews: { 'some': { ranting: { gte: value ? parseInt(value) : 0 } } } }
+            addParams('where', _value)
+            break;
+        }
+        case 'other': {
+            const arrValue = (value as string).split(',')
+            for (let i = 0, l = arrValue.length; i < l; i++) {
+                const value = arrValue[i];
+                addParams('where', { attribute: { some: { name: value } } } as never)
+            }
+            break;
+        }
+        case 'id':
+        case 'discount':
+        case 'views':
+        case 'price':
+        case 'quantity':{
+            const res = getWhereParamsCons(value, getWhereParamsForInt)
+            if (res.error.length) {
+                res.error.forEach(_ => addError(_))
+            } else {
+                // console.log(res.data)
+                addParams('where', { [key]: res.data === value ? Number(res.data) : res.data })
+            }
+            break;
+        }
+        case 'name':
+        case 'art':
+        case 'description':
+        case 'itemArt':
+        case 'itemMod':{
+            const res = getWhereParamsCons(value, getWhereParamsForString)
+            if (res.error.length) {
+                res.error.forEach(_ => addError(_))
+            } else {
+                addParams('where', { [key]: res.data })
+            }
+            break;
+        }
+        case 'createAt':{
+            const res = getWhereParamsCons(value, getWhereParamsForDate)
+            if (res.error.length) {
+                res.error.forEach(_ => addError(_))
+            } else {
+                addParams('where', { [key]: res.data === value ? new Date(parseInt(res.data)) : res.data })
+            }
+            break;
+        }
+        default: {
+            if (_key.startsWith('chr-')) {
+                const name = key.replace('chr-', '').split('_').join(" ")
+                const arrValue = (value as string).split(',').map(_ => _.split('__').join(' '))
+                const params: Prisma.Enumerable<Prisma.ProductCardWhereInput> = []
+                for (let i = 0, l = arrValue.length; i < l; i++) {
+                    const value = arrValue[i];
+                    params.push({ characteristic: { some: { content: { some: { name: name, value: value } } } } })
                 }
-                case ['price', 'discount', 'views', 'createAt'].includes(key.split('.')[0]): {
-                    const optionKey = key.split('.')
-                    if (optionKey[0] && optionKey[1] && WhereIntFilterKey.includes(optionKey[1] as never)) {
-                        findParams.where.AND.push({
-                            [optionKey[0]]: {
-                                [optionKey[1]]: optionKey[0] === 'createAt' ?
-                                    new Date(value as string) :
-                                    parseFloat(value as string)
-                            }
-                        } as never)
-                    } else if (optionKey[0]) {
-                        findParams.where.AND.push({
-                            [optionKey[0]]: {
-                                gte: optionKey[0] === 'createAt' ?
-                                    new Date(value as string) :
-                                    parseFloat(value as string)
-                            }
-                        } as never)
-                    }
-                    break;
-                }
-                case key.startsWith('chr-'): {
-                    const name = key.replace('chr-', '').split('_').join(" ")
-                    const arrValue = (value as string).split(',').map(_ => _.split('__').join(' '))
-
-                    const params: Prisma.Enumerable<Prisma.ProductCardWhereInput> = []
-                    for (let i = 0, l = arrValue.length; i < l; i++) {
-                        const value = arrValue[i];
-                        params.push({ characteristic: { some: { content: { some: { name: name, value: value } } } } })
-                    }
-                    findParams.where.AND.push({ OR: params } as never)
-                    break;
-                }
-                case key === 'limit': {
-                    findParams.take = parseInt(value as string) as never
-                    break;
-                }
-                case key === 'maker': {
-                    const arrValue = (value as string).split(',')
-                    const params: Prisma.Enumerable<Prisma.ProductCardWhereInput> = []
-                    for (let i = 0, l = arrValue.length; i < l; i++) {
-                        const name = arrValue[i];
-                        params.push({ attribute: { some: { value: { contains: name } } } } as never)
-                    }
-                    findParams.where.AND.push({ OR: params } as never)
-                    break;
-                }
-                case key === 'other': {
-                    const arrValue = (value as string).split(',')
-                    for (let i = 0, l = arrValue.length; i < l; i++) {
-                        const value = arrValue[i];
-                        findParams.where.AND.push({ attribute: { some: { name: value } } } as never)
-                    }
-                    break;
-                }
-                case key === 'page': {
-                    const pageVal = parseInt(value as string)
-                    findParams.skip = pageVal as never
-                    break;
-                }
-                case key === 'search': {
-                    const whereParams: Prisma.ProductCardWhereInput = {
-                        OR: [
-                            { name: { contains: value, mode: 'insensitive' } },
-                            { art: { contains: value, mode: 'insensitive' } },
-                        ]
-                    }
-                    findParams.where.AND.push(whereParams as never)
-                    break;
-                }
-                case key === 'rating': {
-                    const params: Prisma.Enumerable<Prisma.ProductCardWhereInput> = { reviews: { 'some': { ranting: { gte: value ? parseInt(value) : 0 } } } }
-                    findParams.where.AND.push(params as never)
-                    break;
-                }
-                case key.startsWith('orderBy'): {
-                    let finalKey = key.replace(/orderBy\.?/, '')
-                    if (finalKey && ['asc', 'desc'].includes(value)) {
-                        findParams.orderBy.push({ [finalKey]: value } as never)
-                    }
-                    break;
-                }
-                case key.startsWith('where'): {
-                    let replaceKey = key.replace(/where\.?/, '')
-                    if (replaceKey && value) {
-                        let finalKey = replaceKey.split('.')
-                        if (finalKey[1] && WhereIntFilterKey.includes(finalKey[1] as never)) {
-                            if (finalKey[0] && PropertyProductCardInt.includes(finalKey[0] as never)) {
-                                switch (finalKey[1] as unknown as WhereIntFilterKey) {
-                                    case 'in': {
-                                        findParams.where.AND.push({ [finalKey[0]]: { in: [...value.split(',').map((_: string) => parseFloat(_))] } } as never)
-                                        break;
-                                    }
-
-                                }
-                            }
-                        }
-                    }
-                    break;
-                }
+                addParams('where', { OR: params })
+                break;
+            } else {
+                addError({ message: `unknown operator "${key}"` , type: 'error' })
             }
         }
     }
-
-    return findParams
 }
 
-export default defineEventHandler(async (event: H3Event) => {
-    const query = getQuery(event)
 
-    if ('unique' in query) {
-        const findParams = Prisma.validator<Prisma.ProductCardFindManyArgs>()({
-            where: { id: 0 },
-        })
-        for (const key in query) {
-            if (Object.prototype.hasOwnProperty.call(query, key)) {
-                const value = query[key];
-                if (key === 'id') {
-                    findParams.where.id = parseInt(value as string) as never
-                }
-            }
-        }
-        try {
-            const response = await prisma.productCard.findUnique({
-                ...findParams, include: 'fullinfo' in query ? {
-                    image: true,
-                    reviews: {
-                        include: {
-                            user: {
-                                select: {
-                                    id: true,
-                                    name: true,
-                                    username: true,
-                                    profileImage: true,
-                                    createAt: true,
-                                    email: true,
-                                }
-                            }
-                        }
+
+export default defineEventHandler(async (event) => {
+    const query = getQuery(event)
+    try {
+        const resParseData = initFindParams(
+            query as { [k: string]: string },
+            initProductCardFindParams,
+            ElemFullDataKey,
+            keyPropElemRelation
+        )
+
+        if (resParseData.error.length) {
+            return { data: null, error: resParseData.error[0] }
+        } else {
+            if (resParseData.unique) {
+                const resData = await prisma[modelName].findUnique({
+                    where: resParseData.params.where,
+                    select: resParseData.params.select
+                } as never)
+                return { data: resData }
+            } else {
+                const resData = await prisma[modelName].findMany({
+                    ...resParseData.params,
+                    select: {
+                        ...selectProductCard({ 'description': false }),
+                        ...resParseData.params.include,
+                        image: { select: { ...selectImage() } }
                     },
-                    attribute: true,
-                    characteristic: { include: { content: true } }
-                } : {}
-            })
-            if (response) {
-                await prisma.productCard.update({ where: { id: response.id }, data: { views: response.views + 1 } })
-            }
-            return response
-        } catch (error) {
-            console.log(error)
-            return null
-        }
-    } else {
-        try {
-            const findParams = initWhereParams(query)
-            // console.log(findParams)
-            if (findParams.where.AND.length) {
-                const res = await prisma.productCard.findMany({
-                    ...findParams,
-                    skip: (findParams.skip === 0 ? 0 : findParams.skip - 1) * findParams.take,
-                    include: { image: { where: { main: true } }, attribute: true }
-                })
-                if (res.length === findParams.take) {
-                    const nextPageRes = await prisma.productCard.findMany({
-                        ...findParams,
-                        skip: findParams.skip * findParams.take,
+                    skip: resParseData.params.skip <= 0 ? 0 : (resParseData.params.skip - 1) * resParseData.params.take,
+                    include: false
+                } as never)
+                if (resData.length === resParseData.params.take) {
+                    const resDataNext = await prisma[modelName].findMany({
+                        where: resParseData.params.where,
+                        skip: resParseData.params.skip * resParseData.params.take,
+                        take: resParseData.params.take,
                         select: { id: true }
                     })
-
-                    return { data: res, nextPageLength: nextPageRes.length}
+                    return {
+                        data: resData,
+                        nextPageLength: resDataNext.length ? resDataNext.length : 0,
+                        // findParams: {
+                        //     ...resParseData.params,
+                        //     skip: resParseData.params.skip <= 0 ? 0 : (resParseData.params.skip - 1) * resParseData.params.take
+                        // }
+                    }
                 } else {
-                    
-                    return { data: res, nextPageLength: 0 }
+                    return {
+                        data: resData,
+                        nextPageLength: 0,
+                        // findParams: {
+                        //     ...resParseData.params,
+                        //     skip: resParseData.params.skip <= 0 ? 0 : (resParseData.params.skip - 1) * resParseData.params.take
+                        // }
+                    }
                 }
-            } else {
-                return null
             }
-
-        } catch (error) {
-            console.log(error)
-            return null
         }
+
+    } catch (error: any) {
+        console.log(error)
+        return { data: null, error: null }
     }
 })
