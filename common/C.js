1b9954f173b2bedcbde7433e8a1df874c5ce85a0
yura <yuriy.melnichuk.1998@gmail.com>
2024-01-02 01:55:04 +0300

    add common file

---
+++ common/C.js
@@ -0,0 +1,22 @@
+export const BASIC_INFORMATION = {
+    TELEPHONE: '+7 (499) 719-99-94',
+    MAIL: 'info@5kvt.ru',
+    WORKING_HOURS: 'Ежедневно 9:30 - 20:00 (МСК)',
+    ADDRESS: '117218, г. Москва, пр-кт Нахимовский .,дом 30/43, кв. 81'
+} 
+
+export const FOOTER = {
+    TELEPHONE: BASIC_INFORMATION.TELEPHONE,
+    MAIL: BASIC_INFORMATION.MAIL,
+    WORKING_HOURS: BASIC_INFORMATION.WORKING_HOURS,
+    ADDRESS: BASIC_INFORMATION.ADDRESS,
+    FIGCAPTION: 'Все права защищены. Указанная стоимость товаров и условия их приобретения действительны на текущую дату.',
+    PAYMENT_METHODS: 'Принимаем к оплате',
+    SOCIAL_NETWORK: 'Напишите нам',
+    BUTTON_TEXT: 'Служба поддержки',
+}
+
+export const HEADER = {
+    TELEPHONE: BASIC_INFORMATION.TELEPHONE,
+    BUTTON_ADDRESS_MARKER: 'Адреса магазинов'
+}
