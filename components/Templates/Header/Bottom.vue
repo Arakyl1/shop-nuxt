68994bc78b1571e1cc392e1c41f0c8ab84fc4c71
yura <yuriy.melnichuk.1998@gmail.com>
2024-03-10 00:03:40 +0300

    update Header Bottom component

--- components/Templates/Header/Bottom.vue
+++ components/Templates/Header/Bottom.vue
@@ -1,57 +1,11 @@
 <template>
     <div class="">
         <Mask :active="state" :fun="update">
-
-        </Mask>
-        <!-- <AtomModalMask :state="state" :clickFun="update" class="" :class="[state ? 'delay-[0]' : 'delay-200 ']">
-            <Transition name="menu-modaile">
-                <div class="relative rounded-md w-min h-min max-h-[90vh] overflow-hidden flex flex-col" ref="menumodal"
-                    @click.stop v-show="state" @mouseleave.stop="onMouseleave">
-
-                    <div class="h-14 lg:h-12 flex bg-blue-500">
-
-                        <AtomButtonStandart @mouseenter.stop="onMouseenter"
-                            class="text-white flex items-center h-full after:content-none py-4">
-                            <CreateIcon :name="'menu_20_20'" class="group is-icon-white mr-3"
-                                :att="{ class: 'fill-white', 'stroke-linecap': 'round' }" />
-                            <NuxtLink to="/catalog">
-                                Каталог
-                            </NuxtLink>
-                        </AtomButtonStandart>
-                    </div>
-
-                    <Transition name="categor">
-                        <div class="flex origin-top-left bg-blue-500 grow overflow-hidden" v-show="state"
-                            ref="contentmodal">
-                            <div class="overflow-y-scroll overflow-x-hidden min-w-min scrollbar-v1">
-                                <MoleculesListSidebar class="pl-4 pr-1.5 w-max" :data="CATEGOR_DATA"
-                                    @mouseout.stop="onMouseout" />
-                            </div>
-                            <div :style="{ maxHeight: heightModal + 'px' }" v-if="groupSubcategor" class="grow">
-                                <div
-                                    class="h-full border-l border-blue-100 py-4 pl-4 pr-1.5 overflow-y-scroll overflow-x-hidden scrollbar-v1">
-                                    <div class="flex flex-row">
-                                        <ul class="w-1/2 flex flex-col min-w-[240px]">
-                                            <li class="mb-5 cursor-pointer w-full" v-for="item in groupSubcategor[0]"
-                                                :key="item.name">
-                                                <MoleculesListAditionalItem :data="item" />
-                                            </li>
-                                        </ul>
-                                        <ul class="w-1/2 flex flex-col min-w-[240px]">
-                                            <li class="mb-5 cursor-pointer w-full" v-for="item in groupSubcategor[1]"
-                                                :key="item.name">
-                                                <MoleculesListAditionalItem :data="item"/>
-                                            </li>
-                                        </ul>
-                                    </div>
-                                </div>
-                            </div>
-                        </div>
-                    </Transition>
-
-                </div>
+            <Transition name="categor">
+                <Menu v-if="state" :style="{ top: positionMenu.top, left: positionMenu.left }"/>
             </Transition>
-        </AtomModalMask> -->
+        </Mask>
+        
         <div class="container">
             <Flex class="h-14 w-full bg-gray-100 relative ">
             <div class="h-full flex" ref="menubutton">
@@ -90,74 +44,40 @@
 import Flex from "@/components/UI/Flex/Flex.vue";
 import Button from "@/components/UI/Button/Button.vue";
 import localState from "@/utils/localState";
-import { CategorDataItem } from "~~/type/intex";
 import Mask from "@/components/UI/Mask/Mask.vue";
+import Menu from './Menu.vue'
+import { CategorDataItem } from "@/type/intex";
 
 const { state, update } = localState()
 // ссылать через свойства el
 const list = ref<InstanceType<typeof Flex> | null | null>(null)
 const active = ref(true)
 const menubutton = ref<HTMLButtonElement | null>(null)
-const menumodal = ref<HTMLElement | null>(null)
-const contentmodal = ref<HTMLElement | null>(null)
-const heightModal = ref<number | null>()
 const className = useCssModule()
-const activeCategor = ref<number | null>(null)
+const positionMenu = reactive({ top: '0px', left: '0px' })
 const CATEGOR_DATA = useState<CategorDataItem[] | null>("CATEGOR_DATA_APP")
-
-
-const groupSubcategor = computed(() => {
-    if (activeCategor.value && CATEGOR_DATA.value) {
-        const findRes = JSON.parse(JSON.stringify(CATEGOR_DATA.value.find(_ => _.id === activeCategor.value)!.children))
-        const lengthGroup = findRes.length / 2
-        const numInteger = Number.isInteger(lengthGroup)
-        return [
-            findRes.filter((_: any, i: number) => i >= 0 && i <= (numInteger ? lengthGroup - 1 : Math.floor(lengthGroup))),
-            findRes.filter((_: any, i: number) => i > (numInteger ? lengthGroup - 1 : Math.floor(lengthGroup)))
-        ]
-    }
-})
 
 
 function onMouseenter(): void {
     if (menubutton.value ) {
         let rect = menubutton.value.getBoundingClientRect()
-        // menumodal.value.style.top = rect.y + 'px' && menumodal.value
-        // menumodal.value.style.left = rect.x + 'px'
-        // let wModal = menumodal.value.clientWidth
-        // menubutton.value.style.minWidth = wModal + 'px'
+        positionMenu.left = transformPx(toFixed(rect.left))
+        positionMenu.top = transformPx(toFixed(rect.top))
         update(true)
-        nextTick(() => {
-            if (contentmodal.value) {
-                heightModal.value = contentmodal.value.clientHeight
-            }
-        })
     }
 }
 
-function onMouseout({ target, type }: MouseEvent) {
-    if (type === 'mouseout') {
-        const elem = target as HTMLElement
-        const elemCategor = elem.closest('[data-categor-id]')
-        if (elemCategor) {
-            const categorId = (elemCategor as HTMLElement).dataset.categorId
-            if (categorId) {
-                activeCategor.value = parseInt(categorId)
-            }
-        }
-    }
-}
+
 
 function onMouseleave(): void {
-    if (menubutton.value && menumodal.value) {
+    if (menubutton.value) {
         update(false)
-        let firstElem = menubutton.value.firstElementChild
-        if (firstElem) {
-            menubutton.value.style.minWidth = firstElem.clientWidth + 'px'
-        }
+        // let firstElem = menubutton.value.firstElementChild
+        // if (firstElem) {
+        //     menubutton.value.style.minWidth = firstElem.clientWidth + 'px'
+        // }
     }
-    heightModal.value = null
-    activeCategor.value = null
+    // activeCategor.value = null
 }
 
 watch(() => state.value, (newV) => {
@@ -206,29 +126,29 @@ function listOffset(): void {
     transform: rotate(-180deg);
 }
 
+
 </style> 
 
 <style lang="css">
 
-.categor-enter-active,
-.categor-leave-active {
-    transition: opacity 0.2s ease;
+.categor-leave-active,
+.categor-enter-active {
+    transition: opacity 0.25s ease-in-out 0.1, transform 0.35s ease-in-out;
 }
 
-.categor-enter-active {
-    transition-delay: 0.2s;
-}
+/* .categor-enter-active {
+    transition-delay: 0.35s;
+} */
 
 .categor-enter-from,
 .categor-leave-to {
     opacity: 0;
-  
-    transform-origin: top;
+    transform-origin: top right;
     transform: scaleY(0);
 }
 
 
-.menu-modaile-enter-active,
+/* .menu-modaile-enter-active,
 .menu-modaile-leave-active {
     transition: opacity 1s ease;
 }
@@ -237,7 +157,7 @@ function listOffset(): void {
 .menu-modaile-leave-to {
     transform-origin: left;
     transform: scaleX(0);
-}
+} */
 
 
 
