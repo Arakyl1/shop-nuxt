359f6fabb0a8e1fa30b246c6168ba830cc13c714
yura <yuriy.melnichuk.1998@gmail.com>
2024-03-13 16:48:55 +0300

    api: add method auth

--- server/api/auth/auth.ts
+++ server/api/auth/auth.ts
@@ -1,27 +1,28 @@
+import { decodeToken } from "@/server/utils/jwt";
 import { _setCookie } from "../../utils/other";
 import { GET_CONTENT_KEY } from "../../utils/other";
-import { CookieKey } from '@/type/intex';
 
 
-export default defineEventHandler(async(event) => {
-
-    const body = await readBody(event)
-    const keyCookie = (k: CookieKey) => k
-    const userRefrechToken = getCookie(event, keyCookie('refrech_token'))
-    const sessionId = getCookie(event, keyCookie('anonim_session_id'))
-
+export default defineEventHandler(async (event) => {
+    let sessionKey = getCookie(event, 'sessionKey')
     try {
-        if (userRefrechToken) {
-            // return {
-            //     access_token: accessToken,
-            //     user: userTransform(user)
-            // }
-        } else {
-            return await authUserAnonim(event, sessionId)
+        if (sessionKey) {
+            const decoded = decodeToken(sessionKey,'refrech')
+            if (decoded === null) new Error(GET_CONTENT_KEY('AUTH_UNAUTHORIZED')) 
+            if (!isObject(decoded)) authError()
+            const res = await $fetch('/api/auth/user',{ params: { id: decoded.id } })
+        
+            return res.user
+        }
+        else {
+            
+            const res = await $fetch('/api/auth/anonim')
+            _setCookie(event, 'sessionKey',res.token)
+            return res.user
         }
     } catch (error) {
-        return { data: null, message: GET_CONTENT_KEY('AUTH_ERROR') }
+        return error
     }
-     
+
 })
 
