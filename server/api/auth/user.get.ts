b53bf0a941da3c2d2ebf3067152a14db9e37ffe3
yura <yuriy.melnichuk.1998@gmail.com>
2024-03-13 16:48:34 +0300

    api: update auth user

--- server/api/auth/user.get.ts
+++ server/api/auth/user.get.ts
@@ -1,23 +1,49 @@
 import prisma from '~/server/db'
 import { userPersonalData, userTransform } from "~/server/utils/userTransform";
-import { decodeAccessToken } from '~~/server/utils/jwt';
 import { isObject } from "../../utils/other";
 import { GET_CONTENT_KEY } from "../../utils/other";
+import { getModelName } from '@/type/intex';
+import { Prisma } from '@prisma/client';
+
+
+type modelName = getModelName<'User'>
+const modelName: Uncapitalize<modelName> = 'user'
+type ThisMainTypeInclude = Prisma.UserInclude
+type ThisMainTypeSelect = Prisma.UserSelect
+
+type keyPropElemRelation = keyof Pick<ThisMainTypeInclude, 'basket'|'refrechToken'|'reviews'|'favorites'>
+const keyPropElemRelation: keyPropElemRelation[] = ['basket','refrechToken','reviews','favorites']
+
+const includeElemSelectParams = {
+    'id': true,
+    'email': true,
+    'name': true,
+    'username': true,
+    'profileImage': true,
+    'createAt': true,
+    'role': true,
+    'basket': { include: { 'item': { include: { 'card': { select: selectProductCard() } } } } },
+    'favorites': { include: { item: { include: { card: { select: selectProductCard() } } } } }
+}
+
+type CheckValid = typeof includeElemSelectParams extends ThisMainTypeInclude ? true : false
+
+
+const _Buyer = Prisma.validator<Prisma.UserArgs>()({ select: includeElemSelectParams })
+export type UserBuyer = Prisma.UserGetPayload<typeof _Buyer>
 
 export default defineEventHandler(async(event) => {
-    
-    const token = getHeader(event, 'authorization')?.split(' ')[1] || ''
-    const decoded = decodeAccessToken(token)
-    if (decoded === null) return { messageKey: GET_CONTENT_KEY('AUTH_UNAUTHORIZED') }
-
+    const query = getQuery(event)
     try {
-        if (!isObject(decoded)) return { messageKey: GET_CONTENT_KEY('AUTH_ERROR') }
+        if ('id' in query && isNumber(query.id)) return authError()
         const user = await prisma.user.findUnique({
-            where: { id: decoded.id },
-            select: userPersonalData.select,
+            where: { id: Number(query.id) as number },
+            select: _Buyer['select'],
         })
-        return user ? { user: userTransform(user) } : { message: GET_CONTENT_KEY('AUTH_ERROR') }
+        if (!user) throw new Error('error')
+       
+        return { user }
     } catch (error) {
-        return { message: GET_CONTENT_KEY('AUTH_ERROR') }
+        return authError()
     }
 })
