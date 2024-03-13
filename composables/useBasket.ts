7e93e47495e086f46eaab3fc6410d5d67c3d53c8
yura <yuriy.melnichuk.1998@gmail.com>
2024-03-13 16:46:36 +0300

    update useBasket [add method remove]

--- composables/useBasket.ts
+++ composables/useBasket.ts
@@ -7,6 +7,17 @@ export default () => {
     const storeUser = _user()
     const { basket } = storeToRefs(storeUser)
 
+    function handleResponse(response: FetchResponse<any> & FetchResponse<ResponseType>) {
+        if (response._data.data) {
+            storeUser.updateBasket(response._data.data)
+        }
+        if (response._data.message) {
+            storeAlert.create({
+                ...response._data.message
+            }) 
+        }
+    }
+
     const add = async(arg: Record<keyof Pick<QueryList, 'card_id'|'count'>, string | number>) => { 
         if (!basket.value) return
 
@@ -14,14 +25,7 @@ export default () => {
             useFetch('/api/basket/toggle', {
                 params: { basket_id: basket.value.id, type: 'add', card_id: arg.card_id, count: arg.count },
                 onResponse({ response }) {
-                    if (response._data.data) {
-                        storeUser.updateBasket(response._data.data)
-                    }
-                    if (response._data.message) {
-                        storeAlert.create({
-                            ...response._data.message
-                        }) 
-                    }
+                    handleResponse(response)
                 }
             })
         } catch (error) {
@@ -29,5 +33,20 @@ export default () => {
         }
     }
 
-    return { add }
+    const remove = async(arg: Record<keyof Pick<QueryList, 'item_id'>, string | number>) => { 
+        if (!basket.value) return
+
+        try {
+            useFetch('/api/basket/toggle', {
+                params: { basket_id: basket.value.id, type: 'remove', item_id: arg.item_id },
+                onResponse({ response }) {
+                    handleResponse(response)
+                }
+            })
+        } catch (error) {
+            console.error(error)
+        }
+    }
+
+    return { add, remove }
 }
