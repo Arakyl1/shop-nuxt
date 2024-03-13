5e5c959a15524703ce1000f05a8da4231e16bd80
yura <yuriy.melnichuk.1998@gmail.com>
2024-03-13 16:46:10 +0300

    update useAuth [delete method data, initAuth]

--- composables/useAuth.ts
+++ composables/useAuth.ts
@@ -1,5 +1,4 @@
 export default () => {
-    const { createAccess, reRefrechAccess } = useToken()
 
     type RegisterData = { email: string, password: string }
     const register = async<T extends RegisterData>(body: T) => { 
@@ -37,42 +36,21 @@ export default () => {
         }
     }
 
-    const data = async<T extends { accessToken: string }>(obj: T) => {
-        try {
-            const data = await $fetch('/api/auth/user', {
-                headers: {
-                    method: "GET",
-                    Authorization: `Bearer ${obj.accessToken}`,
-                },
-            })
-            return data 
-        } catch (error) {
-            console.log(error)
-        }
-    }
+    // const data = async<T extends { accessToken: string }>(obj: T) => {
+    //     try {
+    //         const data = await $fetch('/api/auth/user', {
+    //             headers: {
+    //                 method: "GET",
+    //                 Authorization: `Bearer ${obj.accessToken}`,
+    //             },
+    //         })
+    //         return data 
+    //     } catch (error) {
+    //         console.log(error)
+    //     }
+    // }
 
 
-    const initAuth = async(headers?: any) => { 
-        try {
-            // create new Access Token
-            // const resT = await createAccess();
-            // if (resT && 'accessToken' in resT && resT.accessToken) {
-            //     // Autorization user by token
-            //     const resU = await data(resT)
-            //     if (resU && 'user' in resU && resU.user) {
-            //         reRefrechAccess(resT.accessToken)
-            //     }
-            //     return resU 
-            // } else {
-            //     return resT || null;
-            // }
-            const { data: res } = await useFetch('/api/auth/anonim', { headers })
-             return res.value
-
-        } catch (error) {
-            console.log(error);
-            return null
-        }
-    }
-    return { login, logout, register, initAuth }
+    
+    return { login, logout, register }
 }
